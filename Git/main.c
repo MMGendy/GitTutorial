1afc06f0d7c632fafa3770b9dcd1dee74ebbe586
MarttinaGendy <marttina2912@gmail.com>
2023-11-26 20:08:35 -0800

    Initial Commit

---
+++ Git/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Git
+//
+//  Created by Marttina Gendy on 11/26/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
